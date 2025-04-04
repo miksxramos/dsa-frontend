bae4e540d3015b66660fdea8cdb0eb10c80bda1f
StorThiago <tbpsa@iscte-iul.pt>
2025-04-04 19:23:56 +0100

    feat: insert NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
+import Perfil from './pages/Perfil.jsx';
+
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Navbar from './components/Navbar.jsx';
+
 import './assets/css/styles.css';
 
-export default function App() {
+function App() {
   return (
     <Router>
       <Header />
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/perfil" element={<Perfil />} />
+        </Routes>
       <Footer />
     </Router>
   );
 }
+export default App;
+
+
+// export default function App() {
+//   return (
+//     <Router>
+//       <Header />
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//       </Routes>
+//       <Footer />
+//     </Router>
+//   );
+// }
